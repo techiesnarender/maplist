c3e6518baad6c4aaedaf96edf49e935b1a4c19b2
ayush989999 <ayush.s@mipl.us>
2022-12-20 10:35:19 +0530

    mapload

--- src/clist.js
+++ src/clist.js
@@ -1,46 +1,110 @@
-import { Container, Row,Col } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap"
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
+import Updateform from "./updateform";
 
-function CompanyList(){
-    return(<>
-    <Container>
-        <Row>
-            <Col>
-            <h1 className="text-center mt-4">This is Company List</h1>
-            <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-            </Col>
+function CompanyList() {
+  const [data, setdata] = useState([]);
+  const [load,setLoad] = useState(false)
+  const values = [true];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
 
-        </Row>
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  function getList(){
+    fetch("https://tomcat1.shiftescape.com/api/users").then((response) => {
+      setLoad(false);
+      response.json().then(((result) => {
+        //console.log(result)
+        setdata(result);
+        
+      }))
+    }).catch(e => {
+     // console.log(e);
+      setLoad(false);
+    });
+  }
+
+  function deleteUser(id){
+    fetch(`https://tomcat1.shiftescape.com/api/users/delete/${id}`,{
+      method:"POST"
+    }).then((response)=>{
+      console.log(response)
+    })
+
+  }
+
+  useEffect(() => {
+    setLoad(true);
+    getList();
+    deleteUser();
+  }, [])
+
+  return (<>
+    <Container fluid>
+      <Row>
+        <Col>
+          <h1 className="text-center mt-4">Company List</h1>
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+              <th>ID</th>
+              <th>S.No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Company  Name</th>
+                <th>Address</th>
+                <th>Time</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {load && (
+                <span className="spinner-border spinner-border-sm"></span>
+              )}
+            <tbody>
+              {
+                data.map((item, index) =>
+
+                  <tr key={index}>
+                     <td>{index+1}</td>
+                    <td>{item.id}</td>
+                    <td>{item.contactname}</td>
+                    <td>{item.email}</td>
+                    <td>{item.company}</td>
+                    <td>{item.address}</td>
+                    <td>{item.open} - {item.close}</td>
+                    <td><Button onClick={()=>deleteUser(item.id)}>Delete</Button> <span>&nbsp;</span>
+                    {values.map((v, idx) => (
+        <Button key={idx} className="me-2" onClick={() => handleShow(v)}>
+          Update
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Updateform/>
+        </Modal.Body>
+      </Modal>
+                    </td>
+                  </tr>
+                )
+              }
+            </tbody>
+          </Table>
+        </Col>
+
+      </Row>
+    
     </Container>
-    </>)
+  </>)
 }
 
 export default CompanyList
---
+++ src/updateform.js
@@ -0,0 +1,310 @@
+/*global google*/
+import React, { useState, useEffect, useRef } from 'react';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+
+function Updateform() {
+
+  const [validated, setValidated] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [compnay, setCompnany] = useState("");
+  const [address, setAddress] = useState("");
+  const [open, setOpen] = useState("");
+  const [close, setClose] = useState("")
+  const [dlatitude, setDlatitude] = useState("");
+  const [dlongitude, setDlongitude] = useState("");
+  const [load,setLoad] = useState(false);
+  const [check,setCheck] = useState();
+
+  var ch = document.getElementById("checked");
+  function sub(){
+    if(ch.checked){
+      
+      var json = {
+        "contactname": name,
+        "email": email,
+        "password": password,
+        "company": compnay,
+        "address": address,
+        "latitude": dlatitude,
+        "longitude": dlongitude,
+        "open": open,
+        "close": close,
+        "chargesperhour": null,
+        "logo": null,
+        "enabled": true,
+        "resetPasswordToken": null
+      }
+      setLoad(true);
+      fetch("https://tomcat1.shiftescape.com/api/users",{
+        method : "POST",
+        headers: {
+          "Accept": "application/json",
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        body : JSON.stringify(json)
+      }).then((respons) => {
+        setLoad(false);
+        respons.json().then((result) => {
+          
+        })
+      }).catch(e => {
+        console.log(e);
+        setLoad(false);
+      });
+      console.log(json)
+     }else{
+     
+      alert("Please checked Agree to terms and conditions")
+     }
+    
+  }
+
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  // Javascript Map Call strat
+  const effectRan = useRef(false);
+  useEffect(() => {
+    if (effectRan.current === false) {
+      const script = document.createElement('script');
+
+      script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAfL2oULbnrWbl_G-WdVcxGH8TfEme8dhk&callback=initAutocomplete1&libraries=places&v=weekly";
+      script.async = true;
+      document.body.appendChild(script);
+      return () => {
+        document.body.removeChild(script);
+        effectRan.current = true;
+      }
+    }
+  }, []);
+  /**
+    * @license
+    * Copyright 2019 Google LLC. All Rights Reserved.
+    * SPDX-License-Identifier: Apache-2.0
+    */
+
+
+  useEffect(() => {
+    function initAutocomplete1() {
+        const map = new google.maps.Map(document.getElementById("map1"), {
+          center: {
+            lat: -33.8688,
+            lng: 151.2195
+          },
+          zoom: 13,
+          mapTypeId: "roadmap",
+        });
+        // Create the search box and link it to the UI element.
+        const input = document.getElementById("pac-input");
+        const searchBox = new google.maps.places.SearchBox(input);
+    
+        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener("bounds_changed", () => {
+          searchBox.setBounds(map.getBounds());
+        });
+    
+        let markers = [];
+    
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener("places_changed", () => {
+          const places = searchBox.getPlaces();
+    
+          if (places.length === 0) {
+            return;
+          }
+          var addre = places[0].formatted_address
+          console.log(places[0].formatted_address)
+          setAddress(addre)
+          // Clear out the old markers.
+          markers.forEach((marker) => {
+            marker.setMap(null);
+          });
+          markers = [];
+    
+    
+          // For each place, get the icon, name and location.
+          const bounds = new google.maps.LatLngBounds();
+    
+          places.forEach((place) => {
+            if (!place.geometry || !place.geometry.location) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+    
+            const icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25),
+            };
+    
+            // Create a marker for each place.
+            markers.push(
+              new google.maps.Marker({
+                map,
+                //icon,
+                title: place.name,
+                position: place.geometry.location,
+                draggable: true,
+              })
+            );
+            console.log(markers[0])
+    
+            google.maps.event.addListener(markers[0], 'dragend', function (a) {
+              console.log(a);
+              setDlatitude(a.latLng.lat())
+              setDlongitude(a.latLng.lng())
+            });
+    
+    
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+    
+          map.fitBounds(bounds);
+          let infoWindow = new google.maps.InfoWindow({
+          });
+    
+          infoWindow.open(map);
+    
+        });
+      }
+    
+      window.initAutocomplete1 = initAutocomplete1;
+  }, []);
+
+
+  // Javascript Map Call strat
+  return (<>
+
+    <Container>
+      <Row>
+        <Col>
+          <h1 className='mt-4 text-center'>This is Registration</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="4" controlId="validationCustom01">
+                
+                <Form.Label>First name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="First name"
+                  defaultValue="Mark"
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom02">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  required
+                  type="email"
+                  placeholder="Last name"
+                  defaultValue="Otto"
+                  onChange={(e) => setMail(e.target.value)}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                <Form.Label>Password</Form.Label>
+                <InputGroup hasValidation>
+
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                    onChange={(e)=>setPassword(e.target.value)}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please choose a Password.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="4" controlId="validationCustom03">
+                <Form.Label>Company Name</Form.Label>
+                <Form.Control
+                 type="text"
+                 placeholder="Company Name"
+                 required
+                 onChange={(e)=>setCompnany(e.target.value)}
+                 />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid Company Name.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom04">
+                <Form.Label>Opening Hour's</Form.Label>
+                <Form.Control type="time" placeholder="Opening Hour's" required onChange={(e)=>setOpen(e.target.value)} />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid Opening Hour's.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom05">
+                <Form.Label>Closes Hour's</Form.Label>
+                <Form.Control type="time" placeholder="Closes Hour's" required onChange={(e)=>setClose(e.target.value)}/>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid Closes Hour's.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} md="12" controlId="validationCustom05">
+                <Form.Label>Address </Form.Label>
+                <Form.Control id="pac-input" type="text" placeholder="Address " required onChange={(e)=>setAddress(e.target.value)}/>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid Address .
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Col>
+                <div id="map1" style={{ width: "100%", height: "200px", borderRadius: "15px",marginTop:"15px",boxShadow:"0px 0px 5px #ddd"}}></div>
+              </Col>
+            </Row>
+            <Form.Group className="mt-4 mb-3">
+              <Form.Check
+                id="checked"
+                required
+                label="Agree to terms and conditions"
+                feedback="You must agree before submitting."
+                feedbackType="invalid"
+                value="true"
+                onChange={(e)=>setCheck(e.target.value)}
+              />
+            </Form.Group>
+            <Button onClick={sub}>
+            {load && (
+                <span className="spinner-border spinner-border-sm"></span>
+              )}
+              Submit form</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  </>)
+}
+
+export default Updateform
